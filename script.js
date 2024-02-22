d6bfb63a4fe7dbb0272a9e0b9247d75f11e3d405
lupusergey <sergey.lupu.89@gmail.com>
2024-02-22 22:22:16 +0200

    new

---
+++ script.js
@@ -0,0 +1,18 @@
+let str1 = 'my name';
+let str2 = 'is sergey';
+let str3 = 'i am 34 years old';
+
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
+let concat = str1 + str2 + str3;
+console.log(concat);
+
+
+
+let number = prompt('Пишемо число');
+let result = number.length;
+console.log(result);
+
