09afb77ec542f5e5085260e0e2e2c507fe33b0f1
lupusergey <sergey.lupu.89@gmail.com>
2024-02-26 21:07:18 +0200

    new

---
+++ script.js
@@ -0,0 +1,27 @@
+
+let a = +prompt('число', 2), b = +prompt('число', 2);
+
+alert(a + b);
+
+console.log(a + b);
+
+
+let a1 = +prompt('число', 2), b1 = +prompt('число', 2);
+
+alert(a1 - b1);
+
+console.log(a1 - b1);
+
+
+let a2 = +prompt('число', 2), b2 = +prompt('число', 2);
+
+alert(a2 * b2);
+
+console.log(a2 * b2);
+
+
+let a3 = +prompt('число', 2), b3 = +prompt('число', 2);
+
+alert(a3 / b3);
+
+console.log(a3 / b3);
