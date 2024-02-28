b52df9d8907c81690a4ca37b3720bad5fcb69220
lupusergey <sergey.lupu.89@gmail.com>
2024-02-28 20:37:50 +0200

    new1

---
+++ script.js
@@ -0,0 +1,7 @@
+let year = prompt('рік народження');
+let city = prompt('місто проживання');
+let sport = prompt('улюблений спорт');
+
+console.log('рік народження' + year);
+console.log('місто проживання' + city);
+console.log('улюблений спорт' + sport);
