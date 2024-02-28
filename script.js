a91b0fc60ad323039e4098a144a8491f33c9657f
lupusergey <sergey.lupu.89@gmail.com>
2024-02-28 11:08:22 +0200

    new

---
+++ script.js
@@ -0,0 +1,9 @@
+let num1 = prompt('перше число', 32);
+let num2 = prompt('друге число', 45);
+let num3 = prompt('третє число', 24);
+
+let res = (+num1 * +num2 * +num3);
+
+alert(res);
+
+console.log(res);
