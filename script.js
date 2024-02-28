d78a1a29baf0d2167b0c15d64ccb71c574fdaff4
lupusergey <sergey.lupu.89@gmail.com>
2024-02-28 15:47:29 +0200

    new

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let num1 = prompt('перше число', 32);
-let num2 = prompt('друге число', 45);
-let num3 = prompt('третє число', 24);
+let num1 = parseFloat(prompt('перше число', 32));
+let num2 = parseFloat(prompt('друге число', 45));
+let num3 = parseFloat(prompt('третє число', 24));
 
-let res = (+num1 * +num2 * +num3);
+let average = (+num1 + num2 + num3) / 3;
 
-alert(res);
+alert(average);
 
-console.log(res);
+console.log(average);
