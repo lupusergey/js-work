d3f74163a481e4bffb5b6f5e6cf4369e6ff882c0
lupusergey <sergey.lupu.89@gmail.com>
2024-02-29 21:04:17 +0200

    new

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-let year = prompt('рік народження');
-let city = prompt('місто проживання');
-let sport = prompt('улюблений спорт');
 
-console.log('рік народження' + year);
-console.log('місто проживання' + city);
-console.log('улюблений спорт' + sport);
+let birthYear = prompt('рік народження');
+let city = prompt('місто проживання');
+let favoriteSport = prompt('улюблений спорт');
+
+function getAge() {
+    let age = prompt('Введіть ваш вік:');
+
+    if (!isNaN(age) && age > 0) {
+        alert('Ваш вік: ' + age);
+    } else {
+        alert('введіть коректний вік.');
+    }
+}
+function checkCity(city) {
+    let capitals = {
+        "Київ": "Україна",
+        "Вашингтон": "США",
+        "Лондон": "Велика Британія"
+    };
+    if (capitals.hasOwnProperty(city)) {
+        alert('Ти живеш у столиці ' + capitals[city]);
+    } else {
+        alert('Ти живеш у місті ' + city);
+    }
+}
+getAge();
+
+let userCity = prompt('Введіть назву вашого міста:');
+
+checkCity(userCity);
