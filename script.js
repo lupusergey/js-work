2de63f0a873b356defd27fe595b45506ef001769
lupusergey <sergey.lupu.89@gmail.com>
2024-03-09 12:44:08 +0200

    hew

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ function generateKey(length, characters) {
     }
     return result;
 }
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key);
