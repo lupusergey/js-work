bc234a485d081c1c9052419c1863cc08d8a888fc
lupusergey <sergey.lupu.89@gmail.com>
2024-03-10 12:56:16 +0200

    new

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ function generateKey(length, characters) {
     }
     return result;
 }
-
+/*а тут створили функцію з циклом  всередині  який генерує рядок заданоі довжини.
+кожен символ вибирається випадково за набором characters
+ 
+//  це як я зрозумів мабуть 
+*/
+/*
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 const key = generateKey(16, characters);
 console.log(key);
+//key значить що ми створили рядок з випадковими символами,  тіпа ключ
+//characters що ми створили його з  16 випадкових чисел
+*/
